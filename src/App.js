5245f36b80396123c01218389a9ebf3b0a881b48
Sukma Giri Pratama <sukmagiri11@gmail.com>
2023-01-31 20:39:15 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import styled from 'styled-components';
-import { useEffect } from 'react';
-import { listen } from './app/listener';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { listen } from "./app/listener";
 import { AccountBox } from "./components/accountBox";
-import WithNav from './components/WithNav';
-import WithoutNav from './components/WithoutNav';
-import Home from './pages/Home';
-import Menu from './pages/Home/Menu';
-import Cart from './pages/Cart';
-import Account from './pages/Account';
-import Profile from './components/Profile';
-import Address from './components/Address';
-import Checkout from './pages/Checkout';
-import AddAddress from './components/AddAddress'
-import Order from './components/Order';
-// import Confirmation from './pages/Confirmation';
-// import Invoices from './pages/Invoices';
-
+import WithNav from "./components/WithNav";
+import WithoutNav from "./components/WithoutNav";
+import Home from "./pages/Home";
+import Menu from "./pages/Home/Menu";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
+import Profile from "./components/Profile";
+import Address from "./components/Address";
+import Checkout from "./pages/Checkout";
+import AddAddress from "./components/AddAddress";
+import Order from "./components/Order";
 
 const AppContainer = styled.div`
   width: 100%;
@@ -27,38 +24,40 @@ const AppContainer = styled.div`
   justify-content: center;
 `;
 
-
 const App = () => {
-
-
   useEffect(() => {
     listen();
-  }, [])
+  }, []);
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route element={<WithoutNav />}>
-            <Route path="/Auth" element={<AppContainer><AccountBox /></AppContainer>} />
+            <Route
+              path="/Auth"
+              element={
+                <AppContainer>
+                  <AccountBox />
+                </AppContainer>
+              }
+            />
           </Route>
           <Route element={<WithNav />}>
             <Route path="/" element={<Home />} />
             <Route path="/Menu" element={<Menu />} />
-            <Route path="/cart" element={<Cart />}/>
-            <Route path="/checkout/*" element={<Checkout />}/>
-            {/* <Route path="/invoices:id" element={<Invoices />}/> */}
-            <Route path="/account" element={<Account />}/>
-            {/* <Route path="/confirm" element={<Confirmation />} /> */}
-            <Route path="/profile" element={<Profile />}/>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout/*" element={<Checkout />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/orders" element={<Order />} />
-            <Route path="/address" element={<Address />}/>
-            <Route path="/add-address" element={<AddAddress />}/>
+            <Route path="/address" element={<Address />} />
+            <Route path="/add-address" element={<AddAddress />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
